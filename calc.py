49b233f29a200beb20eb83024727265757188bc6
erfan ashki <erfanbayern111@gmail.com>
2025-02-10 15:19:43 +0330

    create button with for loop from btn_calc_data list

--- calc.py
+++ calc.py
@@ -53,20 +53,27 @@ btn_calc_data = [
     },
 ]
 
+btn_objects = []
+
+for btn_data in btn_calc_data:
+    button = tk.Button(
+        master=window,
+        text=btn_data['text'],
+        command=btn_data['command'],
+        height=3
+    )
+    btn_objects.append(button)
+    
 
 label_calc_result = tk.Label(master=window, text=0, width=30, height=3)
 
 label_calc_result.grid(row=0, column=0, columnspan=4)
 
-btn_7 = tk.Button(master=window, text=7, command=lambda : print(7), height=3)
-btn_8 = tk.Button(master=window, text=8, command=lambda : print(8), height=3)
-btn_9 = tk.Button(master=window, text=9, command=lambda : print(9), height=3)
-btn_plus = tk.Button(master=window, text='+', command=lambda : print('+'), height=3)
 
-btn_7.grid(row=1, column=0, sticky='nsew')
-btn_8.grid(row=1, column=1, sticky='nsew')
-btn_9.grid(row=1, column=2, sticky='nsew')
-btn_plus.grid(row=1, column=3, sticky='nsew')
+# btn_7.grid(row=1, column=0, sticky='nsew')
+# btn_8.grid(row=1, column=1, sticky='nsew')
+# btn_9.grid(row=1, column=2, sticky='nsew')
+# btn_plus.grid(row=1, column=3, sticky='nsew')
 
 
 window.mainloop()
