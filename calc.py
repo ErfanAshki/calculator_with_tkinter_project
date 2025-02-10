ac619e51f8bd9c12aba87e10b5c84c5c39a41cfd
erfan ashki <erfanbayern111@gmail.com>
2025-02-10 15:00:28 +0330

    create btn_calc_data list from data of all buttons

--- calc.py
+++ calc.py
@@ -2,6 +2,58 @@ import tkinter as tk
 
 window = tk.Tk()
 
+btn_calc_data = [
+    {'text': '7',
+    'command': lambda : print('7')
+    }, 
+    {'text': '8',
+    'command': lambda : print('8')
+    }, 
+    {'text': '9',
+    'command': lambda : print('9')
+    },
+    {'text': '+',
+    'command': lambda : print('+')
+    },
+    {'text': '4',
+    'command': lambda : print('4')
+    },
+    {'text': '5',
+    'command': lambda : print('5')
+    },
+    {'text': '6',
+    'command': lambda : print('6')
+    },
+    {'text': '-',
+    'command': lambda : print('-')
+    },
+    {'text': '1',
+    'command': lambda : print('1')
+    },
+    {'text': '2',
+    'command': lambda : print('2')
+    },
+    {'text': '3',
+    'command': lambda : print('3')
+    },
+    {'text': '*',
+    'command': lambda : print('*')
+    },
+    {'text': '0',
+    'command': lambda : print('0')
+    },
+    {'text': '.',
+    'command': lambda : print('.')
+    },
+    {'text': 'C',
+    'command': lambda : print('C')
+    },
+    {'text': '=',
+    'command': lambda : print('=')
+    },
+]
+
+
 label_calc_result = tk.Label(master=window, text=0, width=30, height=3)
 
 label_calc_result.grid(row=0, column=0, columnspan=4)
